1ca014645694d852b81fd7bcd5667c36c5bc3f8d
MikeAriyo <ariyoadekola2@gmail.com>
2023-06-21 13:58:31 +0100

    working with query params

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  #importing
 
-app = FastAPI(); #initializing project
+app = FastAPI(); #creating an instance of fastapi
 
-def index(): 
-    return "heyy"
+@app.get('/blog')  #(/) base path
+
+#path operator function
+def index(limit,published): 
+    #only get 10 published blogs
+
+    if published : 
+        return {"data" : f"{limit} published blogs from the db"}
+    else : 
+         return {"data": f"{limit} blogs from the database"}
+   
+
+
+app.get("/blog/unpublished")
+
+def unpublished ():
+    return {"data": "all unpublished blogs"}
+
+
+#fetching a single blog with id - dynamic routing
+@app.get('/blog/{id}')
+
+def show(id:int): #specifying the types of id we want to return
+    return {'data':
+       id
+    }
+
+
+
+ 
+
+@app.get('/blog/{id}/comments')
+
+def comments(id): 
+    return {"data" : {"1", "2"}}
